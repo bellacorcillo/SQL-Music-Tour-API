5984250141f6b1e0a04ff9344159181e0b11941e
bellacorcillo <isabellacorcillo@gmail.com>
2023-06-26 07:47:54 -0700

    seeders

---
+++ seeders/20230202053750-bands.js
@@ -0,0 +1,23 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('band', [{
+      name: 'The Useful Books',
+      genre: 'Rock',
+      available_start_time: '2023-06-22 19:00:00',
+      end_time: '2023-06-22 23:00:00'
+    },
+    {
+      name: 'U2',
+      genre: 'Alternative',
+      available_start_time: '2023-06-20 19:00:00',
+      end_time: '2023-06-20 23:00:00'
+    }])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    // note that this deletes ALL data from the bands table
+    await queryInterface.bulkDelete('band', null, {})
+  }
+}
